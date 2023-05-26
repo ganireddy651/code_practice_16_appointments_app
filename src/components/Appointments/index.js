624be8e54e028f692db19e42f0d003cee65536ad
root <ganireddy651>
2023-05-26 15:12:24 +0000

    initial commit

---
+++ src/components/Appointments/index.js
@@ -0,0 +1,9 @@
+import {Component} from 'react'
+
+class Appointments extends Component {
+  render() {
+    return <div className="appointments-app-container"></div>
+  }
+}
+
+export default Appointments
